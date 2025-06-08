15c98819dc39d6359a57529f4c0348f85ad4a60d
M450NCH00 <99965994+M450NCH00@users.noreply.github.com>
2025-06-08 23:45:04 +0800

    Add files via upload

--- backend/controllers/ogController.js
+++ backend/controllers/ogController.js
@@ -1,6 +1,12 @@
 import {
     getOGService,
-    updateOGService,
+    updateOGScoreService,
+    updateOGArmyService,
+    updateOGFDCXService,
+    updateOGFDCXPlusService,
+    updateOGMLMFService,
+    updateOGSMMFService,
+    updateOGSMMFPlusService,
     getAllOGService
 } from "../models/ogTableService.js"
 
@@ -36,13 +42,91 @@ export const getAllOG = async(req, res, next) => {
     }
 };
 
-export const updateOG = async(req, res, next) => {
+export const updateOGScore = async(req, res, next) => {
     const {score} = req.body;
     try 
     {
-        const updatedOG = await updateOGService(req.params.id, score);
+        const updatedOG = await updateOGScoreService(req.params.id, score);
         if (!updatedOG) return handleResponse(res, 404, "Not Found!");
-        handleResponse(res, 200, "OG details updated successfully!", updatedOG);
+        handleResponse(res, 200, "OG score updated successfully!", updatedOG);
+    } catch(err)
+    {
+        next(err);
+    }
+};
+
+export const updateOGArmy = async(req, res, next) => {
+    const {army} = req.body;
+    try 
+    {
+        const updatedOG = await updateOGArmyService(req.params.id, army);
+        if (!updatedOG) return handleResponse(res, 404, "Not Found!");
+        handleResponse(res, 200, "OG army updated successfully!", updatedOG);
+    } catch(err)
+    {
+        next(err);
+    }
+};
+
+export const updateOGFDCX = async(req, res, next) => {
+    const {fdcx} = req.body;
+    try 
+    {
+        const updatedOG = await updateOGFDCXService(req.params.id, fdcx);
+        if (!updatedOG) return handleResponse(res, 404, "Not Found!");
+        handleResponse(res, 200, "OG fdcx updated successfully!", updatedOG);
+    } catch(err)
+    {
+        next(err);
+    }
+};
+
+export const updateOGFDCXPlus = async(req, res, next) => {
+    const {fdcx_plus} = req.body;
+    try 
+    {
+        const updatedOG = await updateOGFDCXPlusService(req.params.id, fdcx_plus);
+        if (!updatedOG) return handleResponse(res, 404, "Not Found!");
+        handleResponse(res, 200, "OG fdcx_plus updated successfully!", updatedOG);
+    } catch(err)
+    {
+        next(err);
+    }
+};
+
+export const updateOGMLMF = async(req, res, next) => {
+    const {mlmf} = req.body;
+    try 
+    {
+        const updatedOG = await updateOGMLMFService(req.params.id, mlmf);
+        if (!updatedOG) return handleResponse(res, 404, "Not Found!");
+        handleResponse(res, 200, "OG mlmf updated successfully!", updatedOG);
+    } catch(err)
+    {
+        next(err);
+    }
+};
+
+export const updateOGSMMF = async(req, res, next) => {
+    const {smmf} = req.body;
+    try 
+    {
+        const updatedOG = await updateOGSMMFService(req.params.id, smmf);
+        if (!updatedOG) return handleResponse(res, 404, "Not Found!");
+        handleResponse(res, 200, "OG smmf updated successfully!", updatedOG);
+    } catch(err)
+    {
+        next(err);
+    }
+};
+
+export const updateOGSMMFPlus = async(req, res, next) => {
+    const {smmf_plus} = req.body;
+    try 
+    {
+        const updatedOG = await updateOGSMMFPlusService(req.params.id, smmf_plus);
+        if (!updatedOG) return handleResponse(res, 404, "Not Found!");
+        handleResponse(res, 200, "OG smmf_plus updated successfully!", updatedOG);
     } catch(err)
     {
         next(err);
