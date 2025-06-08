263eab8f126b8e6ddd1e41b4335e137542d1eee0
M450NCH00 <99965994+M450NCH00@users.noreply.github.com>
2025-06-08 23:46:28 +0800

    Add files via upload

--- backend/models/ogTableService.js
+++ backend/models/ogTableService.js
@@ -2,7 +2,7 @@ import pool from "../config/db.js";
 
 export const getOGService = async(id) => {
     const result = await pool.query(
-        "SELECT score FROM og WHERE id = $1",
+        "SELECT score, army, fdcx, fdcx_plus, mlmf, smmf, smmf_plus FROM og WHERE id = $1",
         [id]
     );
     return result.rows[0];
@@ -15,10 +15,58 @@ export const getAllOGService = async() => {
     return result.rows;
 };
 
-export const updateOGService = async(id, score) => {
+export const updateOGScoreService = async(id, score) => {
     const result = await pool.query(
         "UPDATE og SET score = $1 WHERE id = $2 RETURNING *",
         [score, id]
+    );
+    return result.rows[0];
+};
+
+export const updateOGArmyService = async(id, army) => {
+    const result = await pool.query(
+        "UPDATE og SET army = $1 WHERE id = $2 RETURNING *",
+        [army, id]
+    );
+    return result.rows[0];
+};
+
+export const updateOGFDCXService = async(id, fdcx) => {
+    const result = await pool.query(
+        "UPDATE og SET fdcx = $1 WHERE id = $2 RETURNING *",
+        [fdcx, id]
+    );
+    return result.rows[0];
+};
+
+export const updateOGFDCXPlusService = async(id, fdcx_plus) => {
+    const result = await pool.query(
+        "UPDATE og SET fdcx_plus = $1 WHERE id = $2 RETURNING *",
+        [fdcx_plus, id]
+    );
+    return result.rows[0];
+};
+
+export const updateOGMLMFService = async(id, mlmf) => {
+    const result = await pool.query(
+        "UPDATE og SET mlmf = $1 WHERE id = $2 RETURNING *",
+        [mlmf, id]
+    );
+    return result.rows[0];
+};
+
+export const updateOGSMMFService = async(id, smmf) => {
+    const result = await pool.query(
+        "UPDATE og SET smmf = $1 WHERE id = $2 RETURNING *",
+        [smmf, id]
+    );
+    return result.rows[0];
+};
+
+export const updateOGSMMFPlusService = async(id, smmf_plus) => {
+    const result = await pool.query(
+        "UPDATE og SET smmf_plus = $1 WHERE id = $2 RETURNING *",
+        [smmf_plus, id]
     );
     return result.rows[0];
 };
